7a103ce476c32717c0c0a4c261d43028ee727f34
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2018-02-08 10:03:20 -0600

    stays are able to be updated

--- app/controllers/EditStayCtrl.js
+++ app/controllers/EditStayCtrl.js
@@ -1,5 +1,23 @@
 "use strict";
 
-angular.module("OurStayApp").controller("EditStayCtrl", function($scope, FbFactory, FilterFactory, $route){
+angular.module("OurStayApp").controller("EditStayCtrl", function($scope, FbFactory, FilterFactory, $route, $routeParams, $location){
+
+    $scope.title = "Edit Past Stay";
+
+    FbFactory.getStayDetails($routeParams.stayId)
+    .then( (stay) => {
+        $scope.stay = stay;
+    });
+
+    console.log("routParams.stayId:", $routeParams.stayId);
+    console.log();
+
+    $scope.updateStay = function() {
+        FbFactory.updateStayDetails($routeParams.stayId, $scope.stay)
+        .then( () => {
+            console.log("routeParams.stayId");
+            $location.url(`/stay/${$routeParams.stayId}`);
+        });
+    };
 
 });
--- app/controllers/IndividualStayCtrl.js
+++ app/controllers/IndividualStayCtrl.js
@@ -1,5 +1,11 @@
 "use strict";
 
-angular.module("OurStayApp").controller("IndividualStayCtrl", function($scope, FbFactory, FilterFactory, $route){
+angular.module("OurStayApp").controller("IndividualStayCtrl", function($scope, FbFactory, FilterFactory, $route, $routeParams){
 
+    FbFactory.getStayDetails($routeParams.stayId)
+    .then( (stay) => {
+        console.log("stay that should be edited", stay);
+        stay.stayId = $routeParams.stayId;
+        $scope.stay = stay;
+    });
 });
