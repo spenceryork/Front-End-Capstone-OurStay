0699ff7c377f580f4dd45e88ef32915a50a4a644
Spencer York <spencerwyork@gmail.com>
2018-02-08 10:57:10 -0600

    redirect to hotel site no longer changes original url

--- app/controllers/IndividualStayCtrl.js
+++ app/controllers/IndividualStayCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module("OurStayApp").controller("IndividualStayCtrl", function($scope, FbFactory, FilterFactory, $route, $routeParams, $window){
+angular.module("OurStayApp").controller("IndividualStayCtrl", function($scope, FbFactory, FilterFactory, $route, $routeParams, $window, $location){
 
     FbFactory.getStayDetails($routeParams.stayId)
     .then( (stay) => {
@@ -11,6 +11,9 @@ angular.module("OurStayApp").controller("IndividualStayCtrl", function($scope, F
 
     $scope.redirectToSite = function(stayWebsite) {
         console.log("what is staywebsite", stayWebsite);
-        $window.open(stayWebsite);
+        $window.open(stayWebsite, "_blank");
+        console.log("routeParam.stayId", $routeParams.stayId);
+        console.log(`/stay/${$routeParams.stayId}` )
+        $location.url(`/stay/${$routeParams.stayId}`);
     };
 });
