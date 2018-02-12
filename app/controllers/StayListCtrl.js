d25631d85f7f8c272b2ba4b825671001ab30b59e
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2018-02-12 14:51:02 -0600

    sorting on the list page is working correctly

--- app/controllers/StayListCtrl.js
+++ app/controllers/StayListCtrl.js
@@ -4,6 +4,21 @@ angular.module("OurStayApp").controller("StayListCtrl", function($scope, FbFacto
 
     $scope.searchTerm = FilterFactory;
 
+    // Sorting variables - followed along with this tutorial https://www.youtube.com/watch?v=hnKSv28dp_w
+    $scope.sortColumn = 'name';
+    $scope.reverseSort = false;
+    $scope.sortData = function(column) {
+        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+        $scope.sortColumn = column;
+    };
+
+    $scope.getSortClass = function(column) {
+        if ($scope.sortColumn == column) {
+            return $scope.reverseSort ? "arrow-down" : "arrow-up";
+        } 
+        return "";
+    };
+
     let user = firebase.auth().currentUser.uid;
 
     FbFactory.getPastStays(user)
