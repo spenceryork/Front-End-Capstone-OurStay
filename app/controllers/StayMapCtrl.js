3ad3bf98ff60a53c3a6d6c59724f5aff7b55a65c
Spencer York <spencerwyork@gmail.com>
2018-02-12 19:26:01 -0600

    filter by radio almost working on map page

--- app/controllers/StayMapCtrl.js
+++ app/controllers/StayMapCtrl.js
@@ -9,8 +9,31 @@ angular.module("OurStayApp").controller("StayMapCtrl", function($scope, FbFactor
     FbFactory.getPastStays(user)
     .then( (stays) => {
         $scope.stays = stays;
+
+        $scope.getCorrectStays = function() {
+            if ($scope.value === "Hotel") {
+                $scope.CorrectStays = stays.filter( stay =>
+                    stay.type == "Hotel");
+            } else if ($scope.value === "AirBnb") {
+                $scope.CorrectStays = stays.filter( stay =>
+                    stay.type == "AirBnb");
+            } else if ($scope.value === "Campsite") {
+                $scope.CorrectStays = stays.filter( stay =>
+                    stay.type == "Campsite");
+            } else {
+                $scope.CorrectStays = stays;
+            }
+        };
+
+        // console.log("HotelStaysArr", $scope.hotelStaysArr);
         console.log("data received by stayMap controller", stays);
     });
+
+    $scope.getHotels = function() {
+
+    };
+
+
 
 
     $scope.showDetail = function(e, selectedStay) {
